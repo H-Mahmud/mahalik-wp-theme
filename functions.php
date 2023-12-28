bfdfe3503e4a4e93cc2a1b8744109bd4bcba24c5
Mahmudul Hasan <me@imahmud.com>
2023-12-29 02:07:15 +0600

    require fields slider-1

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ require(get_theme_file_path('classes/Nav-Walkers.php'));
 // Theme Functions
 require(get_theme_file_path('inc/setup-theme.php'));
 require(get_theme_file_path('inc/theme-resources.php'));
-
+require(get_theme_file_path('inc/custom-fields.php'));
 
 // shortcodes
+require(get_theme_file_path('shortcodes/product-archive-slide-1.php'));
