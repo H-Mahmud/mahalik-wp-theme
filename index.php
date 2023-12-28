a28796e168f57a009f8e74db4cfe03a2ee5edc01
Mahmudul Hasan <me@imahmud.com>
2023-12-28 08:33:01 +0600

    add index template content

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+<div class="container page-container mb-8 sm:mb-24">
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post();
+            the_content();
 
+        endwhile;
+    else :
+        echo '<h3>' . __('No content found') . '</h3>';
+    endif;
+    ?>
+</div>
 
 <?php get_footer(); ?>
