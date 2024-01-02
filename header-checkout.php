6ee81840eedcdc7fbbec64fcc8666074c194ba28
Mahmudul Hasan <me@imahmud.com>
2024-01-02 18:55:59 +0600

    add checkout header and footer

---
+++ header-checkout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('name'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class('woocommerce-block-theme-has-button-styles color-mode-light rtl font-amazon-ember bg-white topnav-is-dark mainnav-is-light footer-is-dark has-breadcrumbs font-Amazon-Ember'); ?>>
+    <?php wp_body_open(); ?>
